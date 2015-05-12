3a8deb6208e92b61980831cabe85a892a148b98d
Kabir <kabirbaidhya@lftechnology.com>
2015-05-12 11:41:49 +0545

    Configure Doctrine Entity Add Item entity

---
+++ module/Order/src/Order/Entity/Item.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Order\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="items")
+ */
+
+class Item {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     **/
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     **/
+    protected $name;
+    /**
+     * @ORM\Column(type="datetime")
+     **/
+    protected $created;
+    /**
+     * @ORM\Column(type="float")
+     **/
+    protected $rate;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param DateTime $created
+     */
+    public function setCreated(DateTime $created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRate()
+    {
+        return $this->rate;
+    }
+
+    /**
+     * @param float $rate
+     */
+    public function setRate($rate)
+    {
+        $this->rate = $rate;
+    }
+
+}
