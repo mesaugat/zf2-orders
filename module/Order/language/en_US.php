cc6d13f28ffe7537a832e2de2cb43968b8abeb05
Kabir <kabirbaidhya@lftechnology.com>
2015-05-22 10:14:02 +0545

    Validate better with Regex

--- module/Order/language/en_US.php
+++ module/Order/language/en_US.php
@@ -1,8 +1,15 @@
 <?php
 return [
     'no_items' => 'There are no items',
+
+    // Exceptions
     'exception.invalid_page' => 'Invalid Page',
     'exception.page_not_found' => 'Page Not Found',
     'exception.item_not_found' => 'Item Not Found',
     'exception.form_not_bound' => 'The form must be bound to an entity to update',
+
+    // Validations
+    'validation.invalid_item_name' => 'Invalid Item Name',
+    'validation.invalid_item_rate' => 'Invalid Rate',
+
 ];
--- module/Order/src/Order/Filter/ItemFilter.php
+++ module/Order/src/Order/Filter/ItemFilter.php
@@ -2,12 +2,21 @@
 
 namespace Order\Filter;
 
+use Foundation\Traits\TranslatorAwareTrait;
+use Zend\I18n\Translator\TranslatorInterface;
 use Zend\InputFilter\InputFilter;
+use Zend\Validator\Regex;
 
 class ItemFilter extends InputFilter
 {
-    public function __construct()
+    use TranslatorAwareTrait;
+
+    const FLOATING_POINT_PATTERN = '/^\d*(\.\d)?\d*$/';
+    const ITEM_NAME_PATTERN = '/^[a-z]+[a-z0-9\s]+$/i';
+
+    public function __construct(TranslatorInterface $translator)
     {
+        $this->setTranslator($translator);
         $this->initialize();
     }
 
@@ -33,9 +42,18 @@ class ItemFilter extends InputFilter
                     'name' => 'StringLength',
                     'options' => [
                         'encoding' => 'UTF-8',
-                        'min' => 1,
+                        'min' => 3,
                         'max' => 100,
                     ],
+                ],
+                [
+                    'name' => 'Regex',
+                    'options' => [
+                        'pattern' => self::ITEM_NAME_PATTERN,
+                        'messages'  => [
+                            Regex::NOT_MATCH => $this->translate('validation.invalid_item_name')
+                        ]
+                    ]
                 ]
             ],
         ]);
@@ -47,6 +65,17 @@ class ItemFilter extends InputFilter
                 ['name' => 'StripTags'],
                 ['name' => 'StringTrim'],
             ],
+            'validators' => [
+                [
+                    'name' => 'Regex',
+                    'options' => [
+                        'pattern' => self::FLOATING_POINT_PATTERN,
+                        'messages'  => [
+                            Regex::NOT_MATCH => $this->translate('validation.invalid_item_rate')
+                        ]
+                    ]
+                ]
+            ]
         ]);
     }
 
