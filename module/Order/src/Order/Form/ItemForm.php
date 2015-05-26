c20381045fc06e093e139014139c7be715610259
Saugat Acharya <mesaugat@gmail.com>
2015-05-26 09:45:53 +0545

    Use bootstrap classes in form.

--- module/Order/src/Order/Form/ItemForm.php
+++ module/Order/src/Order/Form/ItemForm.php
@@ -18,7 +18,8 @@ class ItemForm extends Form
             'type' => 'Text',
             'options' => [
                 'label' => 'Name',
-            ],
+                'column-size' => 'sm-8'
+            ]
         ]);
 
         $this->add([
@@ -26,16 +27,21 @@ class ItemForm extends Form
             'type' => 'Text',
             'options' => [
                 'label' => 'Rate',
-            ],
+                'column-size' => 'sm-8',
+            ]
         ]);
 
         $this->add([
             'name' => 'submit',
             'type' => 'submit',
-            'attributes' => [
-                'value' => 'Create',
-                'id' => 'submitbutton',
+            'options' => [
+                'label' => 'Submit',
+                'column-size' => 'sm-8 col-sm-offset-2'
             ],
+            'attributes' => [
+                'id' => 'submitbutton',
+                'class' => 'btn-primary'
+            ]
         ]);
     }
 }
