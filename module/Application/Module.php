19985b3f3dd4354d897bd14d1f25dfe4f448af2f
Kabir <kabirbaidhya@lftechnology.com>
2015-05-29 16:53:50 +0545

    Try to fix bjyauthorize acl issue

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Application;
 
+use Zend\Console\Console;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 use Zend\View\Helper\Navigation;
@@ -21,15 +14,16 @@ class Module
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
 
-
         $sm = $e->getApplication()->getServiceManager();
 
         // Add ACL information to the Navigation view helper
-        $authorize = $sm->get('BjyAuthorize\Service\Authorize');
-        $acl = $authorize->getAcl();
-        $role = $authorize->getIdentity();
-        Navigation::setDefaultAcl($acl);
-        Navigation::setDefaultRole($role);
+        if (!Console::isConsole()) {
+            $authorize = $sm->get('BjyAuthorize\Service\Authorize');
+            $acl = $authorize->getAcl();
+            $role = $authorize->getIdentity();
+            Navigation::setDefaultAcl($acl);
+            Navigation::setDefaultRole($role);
+        }
     }
 
     public function getConfig()
